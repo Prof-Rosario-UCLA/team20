c7ba6d04c85698cd0f19c98ca9778b2d20f8d180
yuridekim <cerisehill@korea.ac.kr>
2025-05-11 11:40:02 -0700

    Initial commit for CS144 final project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Academic Scholars Profile Explorer</h1>
+      </header>
+      <p>Welcome to the Academic Scholars Profile Explorer.</p>
+      <p>Search your scholar of interest to explore their research.</p>
+    </div>
+  );
+}
+
+export default App;
